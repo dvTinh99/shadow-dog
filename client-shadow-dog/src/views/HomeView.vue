087c7e104893eaddcada1bc472c159dce8e7dfb9
dvtinh99 <tinh.doan@codecomplete.jp>
2024-12-25 14:20:56 +0700

    update UI

--- client-shadow-dog/src/views/HomeView.vue
+++ client-shadow-dog/src/views/HomeView.vue
@@ -75,20 +75,14 @@
         </div>
       </div>
       <div id="bet-container">
-        <div class="row">
-
-          <button type="button" class="col-6">deer</button>
-          <button type="button" class="col-6">bear</button>
-        </div>
-        <div class="row">
-        
-          <button type="button" class="col-6">hippo</button>
-          <button type="button" class="col-6">giraffe</button>
-        </div>
-        <div class="row">
-          <button type="button" class="col-6">zebra</button>
-          <button type="button" class="col-6">elephant</button>
-        </div>
+          <button type="button" class="btn btn-primary">5.000</button>
+          <button type="button" class="btn btn-success">10.000</button>
+          <button type="button" class="btn btn-info">20.000</button>
+          <button type="button" class="btn btn-secondary">50.000</button>
+          <button type="button" class="btn btn-success">100.000</button>
+          <button type="button" class="btn btn-info">200.000</button>
+          <button type="button" class="btn btn-danger">1.000.000</button>
+          <button type="button" class="btn btn-dark">2.000.000</button>
       </div>
 
       <div class="chat-box">
@@ -211,7 +205,10 @@
 #bet-container {
   box-sizing: border-box;
   padding: 10px;
-  gap: 10px;
+  display: grid;
+  row-gap: 17px;
+  column-gap: 15px;
+  grid-template-columns: auto auto auto auto;
 }
 
 #canvas1 {
