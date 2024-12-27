479550a6bf5728e86dda970a6c5bf75f56d6db87
dvtinh99 <tinh.doan@codecomplete.jp>
2024-12-27 09:50:40 +0700

    prevent open dev tool

--- client-shadow-dog/src/App.vue
+++ client-shadow-dog/src/App.vue
@@ -1,5 +1,68 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
 import { RouterLink, RouterView } from 'vue-router'
+var tryCount = 0;
+var minimalUserResponseInMiliseconds = 200;
+
+onMounted(() => {
+  check();
+
+  window.onload = function () { 
+    document.addEventListener("contextmenu", function (e) { 
+        e.preventDefault();
+    }, false);
+    document.addEventListener("keydown", function (e) {
+        // Ctrl+Shift+I 
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 73) { 
+            disabledEvent(e);
+        }
+        // Ctrl+Shift+J 
+        if (e.ctrlKey && e.shiftKey && e.keyCode == 74) { 
+            disabledEvent(e);
+        } 
+        // Ctrl+S 
+        if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) { 
+            disabledEvent(e);
+        }
+        // Ctrl + U 
+        if (e.ctrlKey && e.keyCode == 85) { 
+            disabledEvent(e);
+        }
+        // F12
+        if (event.keyCode == 123) { 
+            disabledEvent(e);
+        } 
+    }, false);
+    function disabledEvent(e) { 
+        if (e.stopPropagation) { 
+            e.stopPropagation();
+        } else if (window.event) { 
+            window.event.cancelBubble = true;
+        } 
+        e.preventDefault();
+        return false;
+    }
+};
+})
+function check() { 
+    console.clear();
+    let before = new Date().getTime();
+    debugger;
+    let after = new Date().getTime();
+    if (after - before > minimalUserResponseInMiliseconds) { 
+        document.write(" Dont open Developer Tools. ");
+        self.location.replace(
+            window.location.protocol + window.location.href.substring(
+                window.location.protocol.length
+            )
+        );  
+    }
+    setTimeout(check, 100);
+}
+
+
+
+
 </script>
 
 <template>
